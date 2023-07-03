809b60877c19c74d0cfd4f8365fd27e01a7a09ed
sirichodavarapu <sirichodavarapu059@gmail.com>
2023-07-03 16:10:52 +0530

    added files

---
+++ DataLayer/StoredProcedures.cs
@@ -0,0 +1,12 @@
+﻿namespace DataLayer
+{
+    public static class StoredProcedures
+    {
+        public const string CreateUser = "[CreateUser]";
+        public const string getEquipments = "[getEquipments]";
+        internal static string getStatus;
+        //create stored procedure for ValdiateUser
+        public const string ValidateUSer = "[ValidateUser]";
+        
+    }
+}
